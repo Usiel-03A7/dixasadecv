b93e38fbcac98531bc54f3a7bda74278248c9c33
Usiel-03A7 <shebastyan@gmail.com>
2024-04-24 12:00:42 -0600

    finish desing body component

--- src/body/index.jsx
+++ src/body/index.jsx
@@ -2,14 +2,13 @@ import style from "./Body.module.css"
 export default function Body() {
     return (
         <div className={style.container}>
-            <span className={style.containerImg}>
-                <img src="./portada.jpg" alt="portada" />
+            <span className={style.containerTitle}>
+                <h3>Descubre la Excelencia Arquitectónica: Tu Visión, Nuestra Pasión</h3>
             </span>
             <span className={style.containerInfo}>
-                <h3>
-                    Descubre la Excelencia Arquitectónica: Tu Visión, Nuestra Pasión
-                </h3>
-                <p>
+                <img src="./portada.jpg" alt="portada" />
+
+                <p className={style.containerParagraph}>
                     Bienvenido a nuestra firma de arquitectura, donde convertimos tus sueños en estructuras tangibles que inspiran y perduran.
                     En nuestro estudio, cada proyecto es una oportunidad para crear espacios innovadores y funcionales que reflejen la visión
                     única de nuestros clientes.
