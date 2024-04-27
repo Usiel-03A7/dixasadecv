63710e3cb60fb5ff7c7962f0b10c85f2e7d398d3
Usiel-03A7 <shebastyan@gmail.com>
2024-04-27 14:05:46 -0600

    start fine the footer component

--- src/footer/index.jsx
+++ src/footer/index.jsx
@@ -3,7 +3,8 @@ export default function Footer() {
     return (
         <div className={style.container}>
             <span className={style.containerSocial}>
-                <a href="https://www.facebook.com/profile.php?id=100069871142641&mibextid=ZbWKwL" target="_blanck" > pene</a>
+                <a href="https://www.facebook.com/profile.php?id=100069871142641&mibextid=ZbWKwL" target="_blanck" > Facebook</a>
+                <a href="https://www.instagram.com/dixa.dc?igsh=MWFkNGhqbjdlbWU4Yg==" target="_blanck" > instagram</a>
             </span>
 
         </div>
