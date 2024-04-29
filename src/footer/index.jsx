6c0476b79cb5ba207ca2104166cdbcbd47e45dea
Usiel-03A7 <shebastyan@gmail.com>
2024-04-28 19:42:50 -0600

    add footer component

--- src/footer/index.jsx
+++ src/footer/index.jsx
@@ -3,10 +3,11 @@ export default function Footer() {
     return (
         <div className={style.container}>
             <span className={style.containerSocial}>
+                <h3>Siguenos en nuestras redes</h3>
                 <a href="https://www.facebook.com/profile.php?id=100069871142641&mibextid=ZbWKwL" target="_blanck" > Facebook</a>
                 <a href="https://www.instagram.com/dixa.dc?igsh=MWFkNGhqbjdlbWU4Yg==" target="_blanck" > instagram</a>
             </span>
-            <span >
+            <span className={style.information} >
                 <p>© 2021 DIXA diseñpy contrucción. Todos los derechos reservados.</p>
                 <p> | Política de Privacidad | Términos y Condiciones | <a href="/Contactanos">Contáctanos</a></p>
             </span>
