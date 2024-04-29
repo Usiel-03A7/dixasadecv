4bd956d0047e94f8444c4dcf182e2e4950cedb33
Usiel-03A7 <shebastyan@gmail.com>
2024-04-28 19:34:49 -0600

    continue with the footer component

--- src/footer/index.jsx
+++ src/footer/index.jsx
@@ -6,6 +6,10 @@ export default function Footer() {
                 <a href="https://www.facebook.com/profile.php?id=100069871142641&mibextid=ZbWKwL" target="_blanck" > Facebook</a>
                 <a href="https://www.instagram.com/dixa.dc?igsh=MWFkNGhqbjdlbWU4Yg==" target="_blanck" > instagram</a>
             </span>
+            <span >
+                <p>© 2021 DIXA diseñpy contrucción. Todos los derechos reservados.</p>
+                <p> | Política de Privacidad | Términos y Condiciones | <a href="/Contactanos">Contáctanos</a></p>
+            </span>
 
         </div>
     )
