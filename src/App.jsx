abe6176af9685e3f5c436740244c1e8b75a577f0
Usiel-03A7 <shebastyan@gmail.com>
2024-05-01 03:11:13 -0600

    start to name correct

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,27 @@
 import Navbar from './Navbar';
-import Body from './body';
-import Projects from './projects';
-import MoreProjects from './moreProjects';
-import Footer from './footer';
-import AboutUs from './aboutUs';
-import ContactUs from './contactUs';
-import Portafolio from './portafolio/index,';
+import Body from './Body';
+import Projects from './Projects';
+import MoreProjects from './MoreProjects';
+import Footer from './Footer';
+import AboutUs from './AboutUs';
+import ContactUs from './ContactUs';
+import Portafolio from './Portafolio/index,';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function App() {
 
   return (
     <BrowserRouter >
-
-
-      <Navbar />
       <Routes>
         <Route path='/AboutUs' element={<AboutUs />} />
         <Route path='/ContactUs' element={<ContactUs />} />
         <Route path='/Portafolio' element={<Portafolio />} />
       </Routes>
+      <Navbar />
       <Body />
       <Projects />
       <MoreProjects />
       <Footer />
-
     </BrowserRouter>
   )
 }
