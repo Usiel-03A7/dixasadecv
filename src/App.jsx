6c4f6576001f80c462d945e8d79668af85c0138c
Usiel-03A7 <shebastyan@gmail.com>
2024-05-02 13:12:59 -0600

    make funtional navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
 import Navbar from './Navbar';
-import Body from './Body';
-import Projects from './Projects';
-import MoreProjects from './MoreProjects';
 import Footer from './Footer';
 import AboutUs from './AboutUs';
 import ContactUs from './ContactUs';
-import Portafolio from './Portafolio/index,';
+import Portafolio from './Portafolio';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
 
 export default function App() {
 
   return (
     <BrowserRouter >
+      <Navbar />
       <Routes>
+        <Route path='/' exact element={<Home />} />
         <Route path='/AboutUs' element={<AboutUs />} />
         <Route path='/ContactUs' element={<ContactUs />} />
         <Route path='/Portafolio' element={<Portafolio />} />
+        <Route path='*' element={<Portafolio />} />
       </Routes>
-      <Navbar />
-      <Body />
-      <Projects />
-      <MoreProjects />
+
       <Footer />
     </BrowserRouter>
   )
---
+++ src/Home/index.jsx
@@ -0,0 +1,15 @@
+import Body from "../Body"
+import Projects from "../Projects"
+import MoreProjects from "../MoreProjects"
+
+const Home = () =>{
+    return (
+        <>
+            <Body/>
+            <Projects/>
+            <MoreProjects/>
+        </>
+    )
+}
+
+export default Home
