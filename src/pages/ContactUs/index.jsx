35f8e15416d2cd2049509a39c6ec1096d0680985
Usiel-03A7 <shebastyan@gmail.com>
2024-05-04 14:36:58 -0600

    start contactus page component

--- src/pages/ContactUs/index.jsx
+++ src/pages/ContactUs/index.jsx
@@ -1,7 +1,81 @@
+
+import { useState } from "react"
+import style from "./contactUs.module.css"
 export default function ContactUs() {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        number: "",
+        mail: "",
+        subject: "",
+        textArea: ""
+    })
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            name, value
+        });
+        console.log(formData);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+
+        setFormData({
+            name: '',
+            number: '',
+            mail: '',
+            subject: '',
+            textArea: ''
+        })
+    }
+
     return (
-        <div>
-            <h2>Hola estas en el contactanos</h2>
+        <div className={style.container}>
+            <span></span>
+            <span></span>
+            <span>
+                <form className={style.containerFrom} onSubmit={handleSubmit} >
+                    <input
+                        type="text"
+                        placeholder="Ingresa tu nombre completo"
+                        name={name}
+                        onChange={handleInputChange} // Asignar el manejador handleInputChange al evento onChange del campo
+                    />
+                    <input
+                        type="number"
+                        placeholder="Ingresa tu Numero"
+                        name="number"
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        type="email"
+                        placeholder="Ingresa tu Correo"
+                        name="mail"
+                        onChange={handleInputChange}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Ingresa el asunto"
+                        name="subject"
+                        onChange={handleInputChange}
+                    />
+                    <button type="submit">Enviar</button> {/* Usar el tipo "submit" para que el formulario se envíe cuando se haga clic en el botón */}
+                </form>
+            </span>
+            <span className={style.containermap}>
+                <h2>Estamos aquí</h2>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235.38887581669294!2d-103.7353529709047!3d19.272709164800386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8425452dae4398a1%3A0x4c345c95c502315a!2sPapeler%C3%ADa%20e%20impresiones%20ARQUI!5e0!3m2!1ses-419!2smx!4v1714843148771!5m2!1ses-419!2smx"
+                    width="600"
+                    height="450"
+                    style={{ border: 0 }}
+                    allowFullScreen=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+            </span>
         </div>
     )
 }
