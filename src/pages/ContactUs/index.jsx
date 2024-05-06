ebe9219aa2b23a7adb2bc9228948e33866a46218
Usiel-03A7 <shebastyan@gmail.com>
2024-05-06 14:58:07 -0600

    add the new form

--- src/pages/ContactUs/index.jsx
+++ src/pages/ContactUs/index.jsx
@@ -43,9 +43,12 @@ export default function ContactUs() {
                 <h2>Arquitectas</h2>
                 <h3>Mayra Paola Moreno Jerez</h3>
                 <h3>Junnyfer Nomeacuerdo Sunombre Peroluegolopongo</h3>
+                <img src="./portada.jpg" alt="portada" />
             </span>
-            <span>
+
+            <span className={style.containerFormMap}>
                 <form className={style.containerFrom} onSubmit={handleSubmit} >
+                    <h2 >Mandanos tus dudas</h2>
                     <input
                         type="text"
                         placeholder="Ingresa tu nombre completo"
@@ -70,20 +73,25 @@ export default function ContactUs() {
                         name="subject"
                         onChange={handleInputChange}
                     />
+                    <textarea placeholder="Ingrese el mensaje a enviar"
+                        type="text"
+                        name="textArea"
+                        onChange={handleInputChange}
+                    ></textarea>
                     <button type="submit">Enviar</button> {/* Usar el tipo "submit" para que el formulario se envíe cuando se haga clic en el botón */}
                 </form>
-            </span>
-            <span className={style.containerMap}>
-                <h2>Estamos aquí</h2>
-                <iframe
-                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235.38887581669294!2d-103.7353529709047!3d19.272709164800386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8425452dae4398a1%3A0x4c345c95c502315a!2sPapeler%C3%ADa%20e%20impresiones%20ARQUI!5e0!3m2!1ses-419!2smx!4v1714843148771!5m2!1ses-419!2smx"
-                    width="600"
-                    height="450"
-                    style={{ border: 0 }}
-                    allowFullScreen=""
-                    loading="lazy"
-                    referrerPolicy="no-referrer-when-downgrade"
-                ></iframe>
+                <span className={style.containerMap}>
+                    <h2>Estamos aquí</h2>
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d235.38887581669294!2d-103.7353529709047!3d19.272709164800386!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8425452dae4398a1%3A0x4c345c95c502315a!2sPapeler%C3%ADa%20e%20impresiones%20ARQUI!5e0!3m2!1ses-419!2smx!4v1714843148771!5m2!1ses-419!2smx"
+                        width="600"
+                        height="450"
+                        style={{ border: 0 }}
+                        allowFullScreen=""
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                </span>
             </span>
         </div>
     )
