3d1af54c24841513ea51cc0f8031f4dab60baa53
Usiel-03A7 <shebastyan@gmail.com>
2024-05-11 13:51:35 -0600

    start email and password

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
 import styles from './Login.module.css';
 
+
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    const email = 'mayramoreno@dixa.com';
+    const password = 'D1x4s4d3c7';
     const [error, setError] = useState(null);
 
     const handleLogin = () => {
@@ -16,31 +17,32 @@ const Login = () => {
             // Aquí puedes realizar la autenticación o enviar los datos a tu servidor
             console.log('Email:', email);
             console.log('Password:', password);
+            alert('El sistema te dice hola mi estimada ARQ: Mayra Moreno')
             // También puedes redirigir al usuario a otra página después de iniciar sesión
         }
     };
 
     return (
-        <div className={styles['login-container']}>
-            <h2 className={styles['login-title']}>Iniciar Sesión</h2>
-            {error && <p className={styles['login-error']}>{error}</p>}
-            <input
-                className={styles['login-input']}
-                type="email"
-                placeholder="Correo electrónico"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-            />
-            <br />
-            <input
-                className={styles['login-input']}
-                type="password"
-                placeholder="Contraseña"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            />
-            <br />
-            <button className={styles['login-button']} onClick={handleLogin}>Iniciar Sesión</button>
+        <div className={styles.container}>
+            <div className={styles['login-container']}>
+                <h2 className={styles['login-title']}>Iniciar Sesión</h2>
+                {error && <p className={styles['login-error']}>{error}</p>}
+                <input
+                    className={styles['login-input']}
+                    type="email"
+                    placeholder="Correo electrónico"
+
+                />
+                <br />
+                <input
+                    className={styles['login-input']}
+                    type="password"
+                    placeholder="Contraseña"
+
+                />
+                <br />
+                <button className={styles['login-button']} onClick={handleLogin}>Iniciar Sesión</button>
+            </div>
         </div>
     );
 
